904de36175f36237c292d629168d8f2b6dffd085
Devon Burriss <devon@chimplab.co>
2019-08-28 10:28:41 +0200

    Adds makeEmail to script file

--- samples/Strings.fsx
+++ samples/Strings.fsx
@@ -9,3 +9,15 @@ module StringBuilder =
     let init() = new StringBuilder()
     let initWith(s:string) = new StringBuilder(s)
     let append (s:string) (sb:StringBuilder) = sb.Append(s)
+
+
+let makeEmail (name) = 
+    let sanitizeString (s:string) = s |> Option.ofObj
+    name
+    |> Option.bind sanitizeString
+    |> Option.orElse (Some "info")
+    |> Option.map (fun n -> sprintf "%s@acme.com" n)
+    |> Option.get
+
+Some "bob" |> makeEmail
+Some null |> makeEmail
